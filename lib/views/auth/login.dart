317256339c46acdb19be0ee6a0325b36f25340c4
m.elamin <mohamedalameen50@gmail.com>
2021-12-07 11:44:58 +0400

    add changes to login design

--- lib/views/auth/login.dart
+++ lib/views/auth/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:getx_starter/config/config.dart';
 import '/utils/validator.dart';
 import '/views/custom/custom-form-field.dart';
 import '/views/custom/custom_button.dart';
@@ -15,43 +17,53 @@ class LoginPage extends GetWidget<AuthViewModel> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const CustomText(loginPageTitle),
-              CustomFormField(
-                hint: usernameHintKey,
-                label: usernameLabelKey,
-                onSaved: (value) => controller.username = value,
-                validator: InputsValidator.usernameValidator,
-              ),
-              CustomFormField(
-                hint: passwordHintKey,
-                label: passwordLabelKey,
-                onSaved: (value) => controller.password = value,
-                validator: InputsValidator.passwordValidator,
-              ),
-              const SizedBox(height: 24.0),
-              AuthButton(
-                text: 'login',
-                onPressed: () => login(context),
-              ),
-              const Spacer(),
-              Row(
-                children: [
-                  const CustomText('You don\'t have account?'),
-                  TextButton(
-                    child: const CustomText('Register Now'),
-                    onPressed: AuthActions.instance.toRegisterPage,
-
-                  )
-                ],
-              )
-            ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SvgPicture.asset(
+                  logoPath,
+                  height: MediaQuery.of(context).size.height / 4,
+                  fit: BoxFit.contain,
+                ),
+                const CustomText(loginPageTitle),
+                Column(
+                  children: [
+                    CustomFormField(
+                      hint: usernameHintKey,
+                      label: usernameLabelKey,
+                      onSaved: (value) => controller.username = value,
+                      validator: InputsValidator.usernameValidator,
+                    ),
+                    CustomFormField(
+                      hint: passwordHintKey,
+                      label: passwordLabelKey,
+                      onSaved: (value) => controller.password = value,
+                      validator: InputsValidator.passwordValidator,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 24.0),
+                AuthButton(
+                  text: 'login',
+                  onPressed: () => login(context),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    const CustomText('You don\'t have account?'),
+                    TextButton(
+                      child: const CustomText('Register Now'),
+                      onPressed: AuthActions.instance.toRegisterPage,
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
