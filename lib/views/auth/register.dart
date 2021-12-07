5c1e8d73f605b9b280573f8b08e7ac0df9b8d52e
m.elamin <mohamedalameen50@gmail.com>
2021-12-07 11:37:54 +0400

    add register logic and validation

--- lib/views/auth/register.dart
+++ lib/views/auth/register.dart
@@ -1,25 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '/utils/validator.dart';
+import '/views/custom/custom-form-field.dart';
 import '/views/custom/custom_button.dart';
 import '/modules/auth/auth.dart';
 import '/config/keys.dart';
 import '/views/custom/custom_text.dart';
 
-class RegisterPage extends StatelessWidget {
-  const RegisterPage({Key? key}) : super(key: key);
+class RegisterPage extends GetWidget<AuthViewModel> {
+  final _formKey = GlobalKey<FormState>();
+  RegisterPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const CustomText(loginPageTitle),
-          AuthButton(
-            text: 'login',
-            onPressed: () => AuthActions.instance.register(context),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CustomText(registerPageTitle),
+              CustomFormField(
+                hint: usernameHintKey,
+                label: usernameLabelKey,
+                onSaved: (value) => controller.newUser.username = value,
+                validator: InputsValidator.usernameValidator,
+              ),
+              CustomFormField(
+                hint: phoneHintKey,
+                label: phoneLabelKey,
+                onSaved: (value) => controller.newUser.phone = value,
+                validator: InputsValidator.phoneValidator,
+              ),
+              CustomFormField(
+                hint: emailHintKey,
+                label: emailLabelKey,
+                onSaved: (value) => controller.newUser.email = value,
+                validator: InputsValidator.emailValidator,
+              ),
+              CustomFormField(
+                hint: passwordHintKey,
+                label: passwordLabelKey,
+                onSaved: (value) => controller.newUser.password = value,
+                validator: InputsValidator.passwordValidator,
+              ),
+              const SizedBox(height: 24.0),
+              AuthButton(
+                text: 'register',
+                onPressed: () => login(context),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
+
+  void login(BuildContext context) {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      AuthActions.instance.signUp(context);
+    }
+  }
+
 }
