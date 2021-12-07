2fca13d467aa18ba201364533f2e37e14b55451f
m.elamin <mohamedalameen50@gmail.com>
2021-12-07 17:07:57 +0400

    restructure code

---
+++ lib/src/views/custom/custom_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '/src/config/config.dart';
+import 'custom_text.dart';
+
+class AuthButton extends StatelessWidget {
+  final String text;
+  final VoidCallback? onPressed;
+  final double width;
+
+  const AuthButton({Key? key, required this.text, this.onPressed, this.width = double.infinity}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: CustomText(text),
+        style: authButtonStyle,
+      ),
+    );
+  }
+}
