552ad2b3fc713592014de134320d9eec8194c263
m.elamin <mohamedalameen50@gmail.com>
2021-12-09 14:52:56 +0400

    remove memory_service.dart

--- lib/mason/bricks/get_x_template/__brick__/src/utils/binding.dart
+++ lib/mason/bricks/get_x_template/__brick__/src/utils/binding.dart
@@ -1,12 +1,10 @@
 import 'package:get/get.dart';
 import '../modules/connections/connection_view_model.dart';
-import '/src/data/services/memory_service.dart';
 
 
 class InitialBindings extends Bindings{
   @override
   void dependencies() {
-    Get.lazyPut(() => MemoryService());
     Get.lazyPut(() => ConnectionViewModel());
   }
 }
