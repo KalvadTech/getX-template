555f2bc1c70dac076e8d2bf75ea9354ad1fc4206
Mohamed50 <Mohamedalameen50@gmail.com>
2023-01-12 14:46:36 +0400

    add isolator function

--- lib/src/utils/get_utils.dart
+++ lib/src/utils/get_utils.dart
@@ -1,5 +1,21 @@
 import 'dart:developer';
+import 'dart:isolate';
 import 'package:get/get.dart';
+
+class Utils {
+
+  Future<T> isolator<T>(T Function() function) async {
+    ReceivePort receivePort = ReceivePort();
+    await Isolate.spawn((message) {
+      T result = function();
+      Isolate.exit(message as SendPort?, result);
+    }, receivePort.sendPort);
+    return await receivePort.first as T;
+  }
+
+}
+
+
 
 void handleError(Response response) {
   if (response.hasError) {
